1350269316916ce1c5eccc9c3d6f362e1be3f6ab
Daud Fauzy Widiyatno <71966505+daudfauzy98@users.noreply.github.com>
2021-03-22 17:31:10 +0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace NumberToWords
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_angka = new System.Windows.Forms.TextBox();
+            this.btn_konv = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_terbilang = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_angka
+            // 
+            this.tb_angka.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_angka.Location = new System.Drawing.Point(19, 41);
+            this.tb_angka.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tb_angka.Name = "tb_angka";
+            this.tb_angka.Size = new System.Drawing.Size(320, 41);
+            this.tb_angka.TabIndex = 0;
+            // 
+            // btn_konv
+            // 
+            this.btn_konv.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_konv.Location = new System.Drawing.Point(345, 41);
+            this.btn_konv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_konv.Name = "btn_konv";
+            this.btn_konv.Size = new System.Drawing.Size(133, 43);
+            this.btn_konv.TabIndex = 1;
+            this.btn_konv.Text = "Konversi";
+            this.btn_konv.UseVisualStyleBackColor = true;
+            this.btn_konv.Click += new System.EventHandler(this.btn_konv_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Masukkan angka (tanpa titik/koma)";
+            // 
+            // lbl_terbilang
+            // 
+            this.lbl_terbilang.AutoSize = true;
+            this.lbl_terbilang.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_terbilang.Location = new System.Drawing.Point(13, 103);
+            this.lbl_terbilang.Name = "lbl_terbilang";
+            this.lbl_terbilang.Size = new System.Drawing.Size(117, 29);
+            this.lbl_terbilang.TabIndex = 3;
+            this.lbl_terbilang.Text = "Terbilang";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 153);
+            this.Controls.Add(this.lbl_terbilang);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_konv);
+            this.Controls.Add(this.tb_angka);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Konversi Angka ke Huruf";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_angka;
+        private System.Windows.Forms.Button btn_konv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_terbilang;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberToWords
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl_terbilang.Visible = false;
+        }
+
+        private void btn_konv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string Number = tb_angka.Text;
+                string Terbilang = KonversiAngka(Number);
+                lbl_terbilang.Text = Terbilang.First().ToString().ToUpper() + Terbilang.Substring(1);
+                lbl_terbilang.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static String Satuan(String Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String Name = "";
+            switch (_Number)
+            {
+                case 1:
+                    Name = "satu"; break;
+                case 2:
+                    Name = "dua"; break;
+                case 3:
+                    Name = "tiga"; break;
+                case 4:
+                    Name = "empat"; break;
+                case 5:
+                    Name = "lima"; break;
+                case 6:
+                    Name = "enam"; break;
+                case 7:
+                    Name = "tujuh"; break;
+                case 8:
+                    Name = "delapan"; break;
+                case 9:
+                    Name = "sembilan"; break;
+            }
+            return Name;
+        }
+
+        private static String Puluhan(String Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String Name = null;
+            switch (_Number)
+            {
+                case 10:
+                    Name = "sepuluh"; break;
+                case 11:
+                    Name = "sebelas"; break;
+                case 12:
+                    Name = "dua belas"; break;
+                case 13:
+                    Name = "tiga belas"; break;
+                case 14:
+                    Name = "empat belas"; break;
+                case 15:
+                    Name = "lima belas"; break;
+                case 16:
+                    Name = "enam belas"; break;
+                case 17:
+                    Name = "tujuh belas"; break;
+                case 18:
+                    Name = "delapan belas"; break;
+                case 19:
+                    Name = "sembilan belas"; break;
+                case 20:
+                    Name = "dua puluh"; break;
+                case 30:
+                    Name = "tiga puluh"; break;
+                case 40:
+                    Name = "empat puluh"; break;
+                case 50:
+                    Name = "lima puluh"; break;
+                case 60:
+                    Name = "enam puluh"; break;
+                case 70:
+                    Name = "tujuh puluh"; break;
+                case 80:
+                    Name = "delapan puluh"; break;
+                case 90:
+                    Name = "sembilan puluh"; break;
+                default:
+                    if (_Number > 0)
+                    {
+                        Name = Puluhan(Number.Substring(0, 1) + "0") + " " + Satuan(Number.Substring(1));
+                    } break;
+            }
+            return Name;
+        }
+
+        private static String KonversiAngka(String Number)
+        {
+            string Word = "";
+            try
+            {
+                bool beginsZero = false;//tests for 0XX    
+                bool isDone = false;//test if already translated    
+                double dblAmt = (Convert.ToDouble(Number));
+                //if ((dblAmt > 0) && number.StartsWith("0"))    
+                if (dblAmt > 0)
+                {//test for zero or digit zero in a numeric    
+                    beginsZero = Number.StartsWith("0");
+
+                    int numDigits = Number.Length;
+                    int pos = 0;//store digit grouping    
+                    String place = "";//digit grouping name:hundres,thousand,etc...    
+                    switch (numDigits)
+                    {
+                        case 1://ones' range    
+
+                            Word = Satuan(Number);
+                            isDone = true;
+                            break;
+                        case 2://tens' range    
+                            Word = Puluhan(Number);
+                            isDone = true;
+                            break;
+                        case 3://hundreds' range    
+                            pos = (numDigits % 3) + 1;
+                            place = " ratus ";
+                            break;
+                        case 4://thousands' range    
+                        case 5:
+                        case 6:
+                            pos = (numDigits % 4) + 1;
+                            place = " ribu ";
+                            break;
+                        case 7://millions' range    
+                        case 8:
+                        case 9:
+                            pos = (numDigits % 7) + 1;
+                            place = " juta ";
+                            break;
+                        case 10://Billions's range    
+                        case 11:
+                        case 12:
+                            pos = (numDigits % 10) + 1;
+                            place = " miliar ";
+                            break;
+                        //add extra case options for anything above Billion...    
+                        default:
+                            isDone = true;
+                            break;
+                    }
+                    if (!isDone)
+                    {
+                        // Untuk angka yang digit ke-1 sampai akhir bukan 0    
+                        if (Number.Substring(0, pos) != "0" && Number.Substring(pos) != "0")
+                        {
+                            try
+                            {
+                                string TempWord;
+                                // Agar pengecekan kondisi hanya berlaku untuk 100 dan 1000
+                                if (Number.Substring(0, 1) == "1" && (numDigits == 3 || numDigits == 4))
+                                {
+                                    place = place.Remove(0, 1);
+                                    TempWord = "se" + place;
+                                }
+                                else
+                                {
+                                    TempWord = KonversiAngka(Number.Substring(0, pos)) + place;
+                                }
+                                Word = TempWord + KonversiAngka(Number.Substring(pos));
+                                Console.WriteLine("Baris 1 tercetak");
+                            }
+                            catch { }
+                        }
+                        else
+                        {
+                            // Untuk angka yang setelah digit ke-1 sampai akhir berupa 0
+                            Word = KonversiAngka(Number.Substring(0, pos)) + KonversiAngka(Number.Substring(pos));
+                        }
+                        
+                        //check for trailing zeros    
+                        //if (beginsZero) word = " and " + word.Trim();    
+                    }
+                    Console.WriteLine("Baris 3 tercetak");
+                    //ignore digit grouping names    
+                    //if (Word.Trim().Equals(place.Trim())) Word = "";
+                }
+            }
+            catch { }
+                        
+            return Word.Trim();
+        }
+    }
+}
